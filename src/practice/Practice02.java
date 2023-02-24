32e85f757e9f3b8beccd9b2530d552d7ae02445d
Jane <ribbon0628@naver.com>
2023-02-24 18:18:25 +0900

    First practice commit

---
+++ src/practice/Practice02.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Practice02 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 사용자에게 섭씨온도를 입력받아서 화씨온도로 변환하여 출력하는 로직을 작성하세요.
+		 변환공식은 직접 찾아서 적용해주세요.
+		 
+		 화씨 온도는 소수점 첫째 자리까지 표현해주세요. 
+		 # ㄹ 한자 7 -> (℃), ㄹ 한자 다음장 4 -> (℉)		
+		 */
+
+	}
+
+}
